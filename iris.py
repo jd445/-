e7651bd6e082274375186bcea3c6c2e25264a727
Junjie Dong <65555729+jd445@users.noreply.github.com>
2021-08-25 23:10:26 +0800

    change

--- iris.py
+++ iris.py
@@ -3,7 +3,10 @@ import sklearn
 import matplotlib
 f = open("iris.data")
 X=[]
+Y_labele = []
+lables = ["Iris-virginica","Iris-versicolor","Iris-setosa"]
 for i in f:
+    Y_labele.append(i.split(",")[4])
     X.append([float(i.split(",")[0].replace("'","")),float(i.split(",")[1].replace("'","")),float(i.split(",")[2].replace("'","")),float(i.split(",")[3].replace("'",""))])
 X = np.array(X)
 f.close()
@@ -11,3 +14,7 @@ print(X)
 from sklearn.cluster import KMeans
 result = KMeans(3).fit_predict(X)
 print(result)
+
+
+
+
