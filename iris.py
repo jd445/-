6e9fe7225dd57cde4560b8c473473bb7a006ba38
Junjie Dong <65555729+jd445@users.noreply.github.com>
2021-08-25 22:54:42 +0800

    cluster problem

--- iris.py
+++ iris.py
@@ -1,6 +1,12 @@
 import numpy as np
 import sklearn
 import matplotlib
-import pandas as pd
-niubi = pd.read_csv("iris.data")
-print(niubi)
+f = open("iris.data")
+X=[]
+for i in f:
+    X.append([float(i.split(",")[0].replace("'","")),float(i.split(",")[1].replace("'","")),float(i.split(",")[2].replace("'","")),float(i.split(",")[3].replace("'",""))])
+X = np.array(X)
+f.close()
+print(X)
+
+nice = sklearn.cluster.KMeans(3).fit(X)
