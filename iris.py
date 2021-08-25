7f1fd57b26469caa0ff7443b2bc9908d1d9dfcc2
Junjie Dong <65555729+jd445@users.noreply.github.com>
2021-08-25 23:31:34 +0800

    done agin

--- iris.py
+++ iris.py
@@ -6,15 +6,15 @@ X=[]
 Y_labele = []
 lables = ["Iris-virginica","Iris-versicolor","Iris-setosa"]
 for i in f:
-    Y_labele.append(i.split(",")[4])
+    Y_labele.append(i.split(",")[4].replace("\n",""))
     X.append([float(i.split(",")[0].replace("'","")),float(i.split(",")[1].replace("'","")),float(i.split(",")[2].replace("'","")),float(i.split(",")[3].replace("'",""))])
 X = np.array(X)
 f.close()
-print(X)
 from sklearn.cluster import KMeans
-result = KMeans(3).fit_predict(X)
-print(result)
+result = KMeans(3).fit(X)
+fuck = result.predict(X)
+center = result.cluster_centers_
 
-
+print(fuck)
 
 
