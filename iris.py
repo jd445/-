56bfdec1e2efb4eee555f11fc6bee14198507b0b
gzy <1242907521@qq.com>
2021-08-26 10:25:06 +0800

    准确率
    我缝合了一份，自己写了点

--- iris.py
+++ iris.py
@@ -1,8 +1,10 @@
 import numpy as np
+import pandas as pd
 import sklearn
 import matplotlib
 f = open("iris.data")
 X=[]
+
 Y_labele = []
 lables = ["Iris-virginica","Iris-versicolor","Iris-setosa"]
 for i in f:
@@ -11,10 +13,24 @@ for i in f:
 X = np.array(X)
 f.close()
 from sklearn.cluster import KMeans
-result = KMeans(3).fit(X)
-fuck = result.predict(X)
+result = KMeans(3).fit_predict(X)
+print(result)
+df = pd.crosstab(result, Y_labele)
+print(df)
+L = []
+for i in range(df.shape[0]):
+    for j in range(df.shape[1]):
+        if i != j:
+            L.append(df.iloc[i, j])
+print(L)
+print("预测准确率为：", round((150 - sum(L)) / 150 * 100, 1), "%")
+
+
+""" fuck = result.predict([np.array([5,3,1,0.5])])
 center = result.cluster_centers_
 
 print(fuck)
+print(center) """
+
 
 
